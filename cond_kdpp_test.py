61254b40b4ff8207ae1cd5d38af2886041889dd2
jrut132 <Joshua18510>
2019-04-29 22:55:30 -0400

    4-29-2019

--- cond_kdpp_test.py
+++ cond_kdpp_test.py
@@ -19,18 +19,19 @@ def cond_kdpp(A,X,k):
 	return idx
 
 
-X = np.random.randn(1000,2)
-print('X[:10]=',X[:10])
+X = np.random.randn(1000,2) 
+rand_idx1 = np.random.choice(len(X),size = 100)
+# print('X[:10]=',X[:10])
 dists = np.linalg.norm(X-np.array([1,1]),axis=1)
 # print('dists.shape=',dists.shape)
-closest_indices = dists.argsort()[:201]
+closest_indices = dists.argsort()[:31]
 
 X_new = np.random.randn(1000,2)+np.array([2.5,2.5])
 # print('X[closest_indices].shape=')
 print('X_new.shape =',X_new.shape)
-idx = cond_kdpp(X[closest_indices],X_new,k=800)
+idx = cond_kdpp(X[closest_indices],X_new,k=70)
 
-plt.scatter(X[:,0],X[:,1],c='b')
+plt.scatter(X[rand_idx1,0],X[rand_idx1,1],c='b')
 plt.title('first plot')
 plt.show()
 plt.figure()
@@ -38,6 +39,12 @@ plt.scatter(X[closest_indices,0],X[closest_indices,1],c = 'b')
 # plt.scatter(X_new[:,0],X_new[:,1],c='r')
 plt.scatter(X_new[idx,0],X_new[idx,1],c='r')
 plt.title('second plot')
+plt.show()
+plt.figure()
+rand_idx = np.random.choice(len(X_new),size = 70)
+print('len(rand_idx)=',len(rand_idx))
+plt.scatter(X_new[rand_idx,0],X_new[rand_idx,1],c='b')
+plt.scatter(X_new[idx,0],X_new[idx,1],c='r')
 plt.show()
 
 
